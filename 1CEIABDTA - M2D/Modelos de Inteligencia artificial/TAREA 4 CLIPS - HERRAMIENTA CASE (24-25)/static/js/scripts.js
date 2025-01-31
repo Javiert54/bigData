b1cbfea9854ab20100fb30396e19bbba7a4d0561
Javiert54 <jdiazmach@gmail.com>
2025-01-31 18:59:24 +0000

    COMMIT

--- 1CEIABDTA - M2D/Modelos de Inteligencia artificial/TAREA 4 CLIPS - HERRAMIENTA CASE (24-25)/static/js/scripts.js
+++ 1CEIABDTA - M2D/Modelos de Inteligencia artificial/TAREA 4 CLIPS - HERRAMIENTA CASE (24-25)/static/js/scripts.js
@@ -9,31 +9,31 @@ let startDragX, startDragY;
 const classes = [];
 const relations = [];
 
-document.getElementById('generate-diagram').addEventListener('click', 
-	async () => { 
-		try { 
-			// Realizar la solicitud POST al servidor Flask 
-			const response = await fetch('/process-diagram', { 
-				method: 'POST', 
-				headers: { 
-					'Content-Type': 'application/json', 
-				}, 
-			}); 
+// document.getElementById('generate-diagram').addEventListener('click', 
+// 	async () => { 
+// 		try { 
+// 			// Realizar la solicitud POST al servidor Flask 
+// 			const response = await fetch('/process-diagram', { 
+// 				method: 'POST', 
+// 				headers: { 
+// 					'Content-Type': 'application/json', 
+// 				}, 
+// 			}); 
 	 
-			// Manejar la respuesta 
-			const result = await response.json(); 
-			const statusDiv = document.getElementById('status'); 
+// 			// Manejar la respuesta 
+// 			const result = await response.json(); 
+// 			const statusDiv = document.getElementById('status'); 
 	 
-			if (response.ok) { 
-				statusDiv.innerHTML = `<p style="color: green;">${result.message}</p>`; 
-			} else { 
-				statusDiv.innerHTML = `<p style="color: red;">Error: ${result.error}</p>`; 
-			} 
-		} catch (error) { 
-			console.error('Error al procesar el diagrama:', error); 
-	        document.getElementById('status').innerHTML = `<p style="color: red;">Error inesperado: ${error.message}</p>`; 
-    } 
-})
+// 			if (response.ok) { 
+// 				statusDiv.innerHTML = `<p style="color: green;">${result.message}</p>`; 
+// 			} else { 
+// 				statusDiv.innerHTML = `<p style="color: red;">Error: ${result.error}</p>`; 
+// 			} 
+// 		} catch (error) { 
+// 			console.error('Error al procesar el diagrama:', error); 
+// 	        document.getElementById('status').innerHTML = `<p style="color: red;">Error inesperado: ${error.message}</p>`; 
+//     } 
+// })
 
 class UMLClass {
     constructor(name, x, y) {
